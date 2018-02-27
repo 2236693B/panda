186be82e63bd4ac01b33aa3e0027a3eb4e229df7
2236693B <2236693B@student.gla.ac.uk>
2018-02-27 16:50:06 +0000

    Fix rango/index.html to panda/index.html in view.py

--- panda/views.py
+++ panda/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(request): 
+def index(request):
 
     context_dict = {}
 
 
-    return render(request, 'rango/index.html', context=context_dict)
+    #return render(request, 'panda/index.html', context=context_dict)
+    return HttpResponse("Hello, world") #Handy test to ensure pyrthon anywhere still working. Remove once render is probably handled
