6223163854785c495cf32f88ba2891a7c77e79a7
2236693B <2236693B@student.gla.ac.uk>
2018-03-07 10:39:50 +0000

    Google Custom search

---
+++ static/js/googleCSE.js
@@ -0,0 +1,13 @@
+(function() {
+    var cx = '016181695351028181325:kvdcqky5mng';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+ })();
+
+
+
+
