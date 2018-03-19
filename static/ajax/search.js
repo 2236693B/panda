99b88b4176287fd9bb63b47866a56dde26fc0e1c
2236693B <2236693B@student.gla.ac.uk>
2018-03-19 22:03:38 +0000

    Bug Fixes

--- static/ajax/search.js
+++ static/ajax/search.js
@@ -9,3 +9,27 @@ function search(model) {
   xhttp.open("GET", ("/panda/search/" + model + "?query=" + query + ""), true);
   xhttp.send();
 }
+
+function loadPlayers(game) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", ("/panda/game/"+ game + "/players/"), true);
+  xhttp.send();
+}
+
+function hide() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content").innerHTML =
+          "";
+    }
+  };
+  xhttp.open("GET", '/panda/reset/', true);
+  xhttp.send();
+}
