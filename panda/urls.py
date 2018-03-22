8697d8e218688265566f1a87e617d030b68181ff
Adam Christie <2237628c@student.gla.ac.uk>
2018-03-22 20:53:12 +0000

    contactUs with views+urls

--- panda/urls.py
+++ panda/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [ url(r'^$', views.index, name='index'),
                 url(r'^about/', views.about, name='about'),
+                url(r'^contact_us/', views.contact_us, name = 'contact us'),
                 url(r'^games/', views.games, name='games'),
 				url(r'^players/', views.players, name='players'),
                 url(r'report/(?P<player_name_slug>[\w\-]+)/$', views.report_player, name='report_player'),
