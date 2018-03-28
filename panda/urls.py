1ec8e65275ec76c84f909b19fceccea4cbabed12
2236693B <2236693B@student.gla.ac.uk>
2018-03-28 14:46:29 +0100

    Slight comment changes

--- panda/urls.py
+++ panda/urls.py
@@ -1,18 +1,21 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [ url(r'^$', views.index, name='index'),
+urlpatterns = [
+                #Base site urls
+                url(r'^$', views.index, name='index'),
                 url(r'^about/', views.about, name='about'),
                 url(r'^contact_us/', views.contact_us, name = 'contact us'),
                 url(r'^games/', views.games, name='games'),
 				url(r'^players/', views.players, name='players'),
-                url(r'report/(?P<player_name_slug>[\w\-]+)/$', views.report_player, name='report_player'),
 
+                #User auth. urls
                 url(r'^login/$', views.user_login, name='login'),
                 url(r'^sign_up/$', views.sign_up, name='sign_up'),
                 url(r'^studio_sign_up/$', views.studio_sign_up, name='studio_sign_up'),
                 url(r'^logout/$', views.user_logout, name='logout'),
 
+                #Game page urls
                 url(r'^game/(?P<game_name_slug>[\w\-]+)/$', views.show_game, name='show_game'),
                 url(r'^game/(?P<game_name_slug>[\w\-]+)/make_rating/$', views.make_game_rating, name='game_rating'),
                 url(r'^game/(?P<game_name_slug>[\w\-]+)/make_comment/$', views.make_game_comment, name='game_comment'),
@@ -25,17 +28,23 @@ urlpatterns = [ url(r'^$', views.index, name='index'),
                 url(r'^game/(?P<game_name_slug>[\w\-]+)/players$', views.get_game_players, name='Player_AJAX'),
                 url(r'^game/(?P<game_name_slug>[\w\-]+)/recommend$', views.recommend_game, name='Recommend_game'),
                 url(r'^game/(?P<game_name_slug>[\w\-]+)/update/$', views.update_game, name='update_game'),
+
+                #Blank page used of AJAX requests
                 url(r'^reset/$', views.reset, name='Reset'),
 
-
+                #Player urls
                 url(r'^player/(?P<player_name_slug>[\w\-]+)/$', views.show_player, name='show_player'),
                 url(r'^player/(?P<player_name_slug>[\w\-]+)/make_rating/$', views.make_player_rating, name='player_rating'),
+                url(r'report/(?P<player_name_slug>[\w\-]+)/$', views.report_player, name='report_player'),
 
+                #Studio page urls
                 url(r'^studio/(?P<studio_name_slug>[\w\-]+)/$', views.show_studio, name='show_studio'),
 
+                #Search urls
                 url(r'^search/games', views.games_search, name='games_search'),
                 url(r'^search/players', views.player_search, name='player_search'),
 
+                #My profile urls
                 url(r'^my_profile/$', views.show_profile, name='my_profile'),
                 url(r'^my_profile/register_game/$', views.register_game, name='register_game'),
                 url(r'^my_profile/edit_player_profile/$', views.edit_player_profile, name='edit_player_profile'),
@@ -44,9 +53,12 @@ urlpatterns = [ url(r'^$', views.index, name='index'),
                 url(r'^my_profile/approve/$', views.approve_player, name='approve_my_profile'),
 
 
+
+                #Forum urls
+
+
                 url(r'^forum/mentioned-users/(?P<topic_id>[-\w]+)/$', views.get_mentioned_user, name="get_mentioned_user"),
 
-                
 
                 url(r'^forum/$', views.TopicList.as_view(), name="topic_list"),
                 url(r'^forum/topic/add/$', views.TopicAdd.as_view(), name="new_topic"),
